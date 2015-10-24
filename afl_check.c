5850e3eace0e07c73a1a370abd69b7448da702d6
Clark Gaebel <cg.wowus.cg@gmail.com>
2015-10-24 10:49:10 -0400

    afl_init

--- afl_check.c
+++ afl_check.c
@@ -12,9 +12,18 @@ int main(int argc, const char *argv[]) {
         }
     }
 
+#ifdef __AFL_HAVE_MANUAL_CONTROL
+  __AFL_INIT();
+#endif
+
+
+#ifdef __AFL_HAVE_MANUAL_CONTROL
     while (__AFL_LOOP(10000)) {
+#endif
         char buf[65536];
         size_t len = fread(buf, 1, sizeof(buf), in);
         nbt_free(nbt_parse(buf, len));
+#ifdef __AFL_HAVE_MANUAL_CONTROL
     }
+#endif
 }
