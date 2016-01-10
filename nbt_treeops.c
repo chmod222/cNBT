2f1981a3c3c55c7a146a5be35d1bc568c1920ec0
realazthat <realazthat@gmail.com>
2016-01-10 04:08:38 -0500

    Renamed __strdup to _nbt_strdup(); was having problems with clang version 3.5.0 on ubuntu

--- nbt_treeops.c
+++ nbt_treeops.c
@@ -15,7 +15,7 @@
 #include <string.h>
 
 /* strdup isn't standard. GNU extension. */
-static inline char* __strdup(const char* s)
+static inline char* _nbt_strdup(const char* s)
 {
     char* r = malloc(strlen(s) + 1);
     if(r == NULL) return NULL;
@@ -121,7 +121,7 @@ clone_error:
 /* same as strdup, but handles NULL gracefully */
 static inline char* safe_strdup(const char* s)
 {
-    return s ? __strdup(s) : NULL;
+    return s ? _nbt_strdup(s) : NULL;
 }
 
 nbt_node* nbt_clone(nbt_node* tree)
@@ -139,7 +139,7 @@ nbt_node* nbt_clone(nbt_node* tree)
 
     if(tree->type == TAG_STRING)
     {
-        ret->payload.tag_string = __strdup(tree->payload.tag_string);
+        ret->payload.tag_string = _nbt_strdup(tree->payload.tag_string);
         if(ret->payload.tag_string == NULL) goto clone_error;
     }
 
@@ -279,7 +279,7 @@ nbt_node* nbt_filter(const nbt_node* tree, nbt_predicate_t filter, void* aux)
 
     if(tree->type == TAG_STRING)
     {
-        ret->payload.tag_string = __strdup(tree->payload.tag_string);
+        ret->payload.tag_string = _nbt_strdup(tree->payload.tag_string);
         if(ret->payload.tag_string == NULL) goto filter_error;
     }
 
